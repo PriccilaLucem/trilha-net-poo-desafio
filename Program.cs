dede4d2cea42d18bd9434fce03b620f49a40f7ad
Priccila Lucem <priccilalucem@gmail.com>
2024-11-03 08:26:04 -0300

    terminando os testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+﻿// Arquivo: Program.cs ou TesteSmartphone.cs (para testes rápidos)
+using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456789", "Nokia Modelo X", "111111111111111", 64);
+        Console.WriteLine("Testando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("987654321", "iPhone Modelo Y", "222222222222222", 128);
+        Console.WriteLine("Testando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
